0c91e35382e6283e1c3146a217b3ae20e34e2c95
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-09-21 22:49:26 +0200

    vox2godot: Add working version

---
+++ vox2godot/common/util.gd
@@ -0,0 +1,100 @@
+
+static func create_wirecube_mesh(color = Color(1,1,1)):
+	var positions = PoolVector3Array([
+		Vector3(0, 0, 0),
+		Vector3(1, 0, 0),
+		Vector3(1, 0, 1),
+		Vector3(0, 0, 1),
+		Vector3(0, 1, 0),
+		Vector3(1, 1, 0),
+		Vector3(1, 1, 1),
+		Vector3(0, 1, 1)
+	])
+	var colors = PoolColorArray([
+		color, color, color, color,
+		color, color, color, color,
+	])
+	var indices = PoolIntArray([
+		0, 1,
+		1, 2,
+		2, 3,
+		3, 0,
+
+		4, 5,
+		5, 6,
+		6, 7,
+		7, 4,
+
+		0, 4,
+		1, 5,
+		2, 6,
+		3, 7
+	])
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = positions
+	arrays[Mesh.ARRAY_COLOR] = colors
+	arrays[Mesh.ARRAY_INDEX] = indices
+	var mesh = ArrayMesh.new()
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arrays)
+	return mesh
+
+
+static func calculate_normals(positions, indices) -> PoolVector3Array:
+	var out_normals = PoolVector3Array()
+	
+	var tcounts = []
+	tcounts.resize(positions.size())
+	out_normals.resize(positions.size())
+	
+	for i in range(0, tcounts.size()):
+		tcounts[i] = 0
+	
+	var tri_count = indices.size() / 3
+	
+	var i = 0
+	while i < indices.size():
+		
+		var i0 = indices[i]
+		var i1 = indices[i+1]
+		var i2 = indices[i+2]
+		i += 3
+		
+		# TODO does triangle area matter?
+		# If it does then we don't need to normalize in triangle calculation since
+		# it will account for its length
+		var n = get_triangle_normal(positions[i0], positions[i1], positions[i2])
+		
+		out_normals[i0] += n
+		out_normals[i1] += n
+		out_normals[i2] += n
+		
+		tcounts[i0] += 1
+		tcounts[i1] += 1
+		tcounts[i2] += 1
+	
+	for j in range(out_normals.size()):
+		out_normals[j] = (out_normals[j] / float(tcounts[j])).normalized()
+	#print("DDD ", out_normals.size())
+	return out_normals
+
+
+static func get_triangle_normal(a: Vector3, b: Vector3, c: Vector3) -> Vector3:
+	var u = (a - b).normalized()
+	var v = (a - c).normalized()
+	return v.cross(u)
+
+
+static func get_longest_axis(v: Vector3) -> int:
+	var lx = abs(v.x)
+	var ly = abs(v.y)
+	var lz = abs(v.z)
+	if lx > ly and lx > lz:
+		return Vector3.AXIS_X
+	if ly > lx and ly > lz:
+		return Vector3.AXIS_Y
+	return Vector3.AXIS_Z
+
+
+static func get_direction_id4(dir: Vector2) -> int:
+	return int(4.0 * (dir.rotated(PI / 4.0).angle() + PI) / TAU)
---
+++ vox2godot/zylann/vox/vox_test.gd
@@ -0,0 +1,77 @@
+extends Node
+
+
+const Util = preload("res://common/util.gd")
+
+const _materials = [
+	preload("./opaque_color_material.tres"),
+	preload("./transparent_color_material.tres")
+]
+
+onready var _mesh_instance : MeshInstance = $MeshInstance
+var _mesher = VoxelMesherCubes.new()
+var _wireframe_bounds : MeshInstance
+
+
+func _ready():
+	var wireframe = Util.create_wirecube_mesh()
+	var wireframe_instance = MeshInstance.new()
+	wireframe_instance.mesh = wireframe
+	_mesh_instance.add_child(wireframe_instance)
+	_wireframe_bounds = wireframe_instance
+
+	_generate()
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			if event.scancode == KEY_G:
+				_mesher.greedy_meshing_enabled = not _mesher.greedy_meshing_enabled
+				print("Greedy meshing: ", _mesher.greedy_meshing_enabled)
+				_generate()
+
+
+func _generate():
+	var voxels = VoxelBuffer.new()
+	voxels.set_channel_depth(VoxelBuffer.CHANNEL_COLOR, VoxelBuffer.DEPTH_8_BIT)
+	var palette = VoxelColorPalette.new()
+	
+	var loader = VoxelVoxLoader.new()
+	var err = loader.load_from_file("res://local_tests/vox/monu1.vox", voxels, palette)
+	if err != OK:
+		push_error(str("Error: ", err))
+		return
+
+	_mesher.palette = palette
+	_mesher.color_mode = VoxelMesherCubes.COLOR_MESHER_PALETTE
+	
+	var padded_voxels = VoxelBuffer.new()
+	padded_voxels.create(
+		voxels.get_size().x + 2, 
+		voxels.get_size().y + 2, 
+		voxels.get_size().z + 2)
+	padded_voxels.set_channel_depth(
+		VoxelBuffer.CHANNEL_COLOR, voxels.get_channel_depth(VoxelBuffer.CHANNEL_COLOR))
+	padded_voxels.copy_channel_from_area(
+		voxels, Vector3(), voxels.get_size(), Vector3(1, 1, 1), VoxelBuffer.CHANNEL_COLOR)
+
+	var time_before = OS.get_ticks_usec()
+	var mesh = _mesher.build_mesh(padded_voxels, _materials)
+	print("Spent ", OS.get_ticks_usec() - time_before, " us to mesh")
+
+	var vertex_count = 0
+	for si in mesh.get_surface_count():
+		var arrays = mesh.surface_get_arrays(si)
+		var vertices = arrays[Mesh.ARRAY_VERTEX]
+		print("Surface ", si, " has ", len(vertices))
+		vertex_count += len(vertices)
+	print("Vertex count: ", vertex_count)
+
+	_mesh_instance.mesh = mesh
+	_wireframe_bounds.scale = voxels.get_size()
+
+
+func _process(delta):
+	#_mesh_instance.rotate_y(delta)
+	pass
